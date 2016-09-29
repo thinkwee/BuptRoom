7bc6e5ddeab5a4f49dd365d99d67ab0f7f147b9b
Thinkwee <thinkwee2767@gmail.com>
2016-09-29 16:48:44 +0800

    BuptRoom
    北邮自习室查询系统

---
+++ app/src/main/java/thinkwee/buptroom/BuptRoomProvider.java
@@ -0,0 +1,49 @@
+package thinkwee.buptroom;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by think on 20162016/9/28 002817:13
+ * PACKAGE:thinkwee.buptroom
+ * PROJECT:BuptRoom
+ * TODO:
+ */
+
+public class BuptRoomProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
